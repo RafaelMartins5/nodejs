7b56d6728925c291a8524389ff82074727c81d36
Rafael Martins <rafaelmartinsorappa@gmail.com>
2022-07-30 14:47:22 -0300

    Node.js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# nodejs
+Aqui aprendi a instalar o express do Noje.js pelo terminal git bash. 
+Aprendi a chamar funcões.
+Aprendi a vincular o HTML e CSS no Index.Js (peguei meu projeto do formulario)
+
+git sta
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require ("express");
+const app= express();
+const port = 5000;
+
+const path = require('path');
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/style.css', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/style.css' ))
+})
+
+app.listen(port,()=>{
+    console.log("servidor rodando");
+})
